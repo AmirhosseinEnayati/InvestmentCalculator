0138aac3df93101b4a2bae220650dc80427e1a61
Amirhossein Enayati <enayati.amirh@gmail.com>
2025-12-19 12:35:21 +0330

    Add header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from "@angular/core";
 import { App } from "./app";
 import { BrowserModule } from "@angular/platform-browser";
+import { Header } from "./header/header";
 
 @NgModule({
-    declarations:[App],
+    declarations:[App, Header],
     bootstrap:[App],
-    imports:[BrowserModule]
+    imports: [BrowserModule]
 })
 export class AppModule{}
